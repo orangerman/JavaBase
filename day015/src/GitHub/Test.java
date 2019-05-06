57046feeb9d7779256379cc72177190a863774f0
java-fan <1057600474@qq.com>
2019-05-06 15:50:03 +0800

    JavaBase代码整合

---
+++ day015/src/GitHub/Test.java
@@ -0,0 +1,11 @@
+package GitHub;
+
+/**
+ * @author java_fan
+ * @create 2019-05-06 15:48
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
