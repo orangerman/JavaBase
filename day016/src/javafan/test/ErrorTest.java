2cb9e057c188e9cd9342e30ede2a73f49e5cb11c
java-fan <1057600474@qq.com>
2019-05-08 14:09:32 +0800

    JavaBase代码整合   测试提交修改文件   提交课后习题   加以   1   可以   2   异常处理

---
+++ day016/src/javafan/test/ErrorTest.java
@@ -0,0 +1,17 @@
+package javafan.test;
+
+/**
+ * @author java_fan
+ * @create 2019-05-08 14:03
+ */
+public class ErrorTest {
+    public static void main(String args){
+        //栈溢出：Java.lang.StackOverflowError
+//        main(args);
+        //堆溢出 Java.lang.OutOfMemoryError
+//        Integer[] arr = new Integer[1024*1024*1024];
+
+
+
+    }
+}
---
+++ day016/src/javafan/test/ExceptionTest.java
@@ -0,0 +1,8 @@
+package javafan.test;
+
+/**
+ * @author java_fan
+ * @create 2019-05-08 14:08
+ */
+public class ExceptionTest {
+}
